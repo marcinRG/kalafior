b34497c6b8789041c6b8155b2d65c5c435916543
MarcinRG <marcin.guszpit@gmail.com>
2022-03-30 15:30:00 +0200

    frontend popups

--- frontend/admin/src/app.js
+++ frontend/admin/src/app.js
@@ -1,10 +1,28 @@
 import './scss/app.scss'
-import {PopUpWindow} from './js/PopUpWindow';
+import {ButtonValues, PopUpWindow} from './js/PopUpWindow';
 // import {AddEventListenerToAllCheckboxes} from './js/AddEventListenerToAllCheckboxes';
 // import {SelectHandlers} from './js/SelectHandlers';
 
 console.log('działa');
+
+export function printSomething(buttonValue) {
+    console.log('button value:');
+    console.log(buttonValue);
+}
 const popUp = new PopUpWindow();
+
+const btn = document.querySelector('.custom-btn');
+btn.addEventListener('click', () => {
+    popUp.showPopUpWindow('Uwaga! To jest okienko', 'To jest jakaś customowa wiadomość', [
+        {
+            title: 'Ok',
+            type: ButtonValues.OK
+        },
+        {
+            title: 'Anuluj',
+            type: ButtonValues.CANCEL
+        }], printSomething);
+});
 
 
 // const checkboxes = new AddEventListenerToAllCheckboxes();
--- frontend/admin/src/js/PopUpWindow.js
+++ frontend/admin/src/js/PopUpWindow.js
@@ -1,17 +1,94 @@
+export const ButtonValues = {
+    OK: 'ok',
+    CANCEL: 'cancel',
+    YES: 'yes',
+    REMOVE: 'remove'
+};
+
 export class PopUpWindow {
     constructor() {
-        this.body = document.querySelector('body');
-        this.div = document.querySelector('.info-wrapper');
-        const scrollTop = document.documentElement.scrollTop;
-        this.div.style.top = scrollTop + 'px';
+        this.value = ButtonValues.CANCEL;
+        this.wrapper = document.querySelector('.info-wrapper');
+        this.info = document.querySelector('.info-screen');
+        this.windowTitle = document.querySelector('.info-msg .info-title');
+        this.message = document.querySelector('.info-msg .message');
+        this.buttonWrapper = document.querySelector('.element-btn-wrapper');
+        this.showInfoAtCurrentScroll = this.showInfoAtCurrentScroll.bind(this);
+        this.showInfoAtCurrentScroll();
+    }
 
-        this.body.append(this.div);
-        console.log(document.documentElement.clientHeight);
-        console.log(document.documentElement.offsetHeight);
-        console.log('doc offsetTop,: ', document.documentElement.offsetTop);
-        console.log('doc, scrollTop: ', document.documentElement.scrollTop);
-        console.log('docu, scrollWidth: ', document.documentElement.scrollWidth)
-        console.log(this.body.clientHeight);
-        console.log(this.body.offsetHeight)
+    setWindowTitle(text) {
+        if (text) {
+            this.windowTitle.textContent = text;
+        }
+    }
+
+    setMessage(text) {
+        if (text) {
+            this.message.textContent = text;
+        }
+    }
+
+    showInfoAtCurrentScroll() {
+        const scrollTop = document.documentElement.scrollTop;
+        this.info.style.top = scrollTop + 'px';
+    }
+
+    show() {
+        const documentHeight = document.documentElement.offsetHeight;
+        this.wrapper.style.height = documentHeight + 'px';
+        this.wrapper.style.display = 'block';
+    }
+
+    hide() {
+        window.removeEventListener('scroll',this.showInfoAtCurrentScroll);
+        this.wrapper.style.display = 'none';
+    }
+
+    createButtons(buttonsProps, func) {
+        this.buttonWrapper.innerHTML = '';
+        for (const button of buttonsProps) {
+            this.buttonWrapper.append(this.createButton(button, func));
+        }
+    }
+
+    createButton(buttonProps, func) {
+        const button = document.createElement('button');
+        button.textContent = buttonProps.title;
+        button.className = getButtonClasses(buttonProps.type)
+        button.addEventListener('click', () => {
+            this.hide();
+            func(buttonProps.type);
+        });
+        return button;
+    }
+
+
+
+    showPopUpWindow(title, message, buttons, func) {
+        this.setWindowTitle(title);
+        this.setMessage(message);
+        this.show();
+        this.showInfoAtCurrentScroll();
+        this.createButtons(buttons, func);
+        window.addEventListener('scroll',this.showInfoAtCurrentScroll);
+    }
+}
+
+export function getButtonClasses(buttonValue) {
+    let className = `button ${ButtonValues[buttonValue]}`;
+    switch (buttonValue) {
+        case ButtonValues.OK: {
+            return `${className} green`
+        }
+        case ButtonValues.CANCEL: {
+            return `${className} white`
+        }
+        case ButtonValues.YES: {
+            return `${className} green`
+        }
+        case ButtonValues.REMOVE: {
+            return `${className} red`
+        }
     }
 }
